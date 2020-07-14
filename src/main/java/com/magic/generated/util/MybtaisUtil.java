7082bb55f91fb0e705eef7fd267a31f40e27725d
zhaoyankai <272852115@qq.com>
2020-07-14 08:01:52 +0800

    Create mybatis SqlSessionFactory with singleton mode

---
+++ src/main/java/com/magic/generated/util/MybtaisUtil.java
@@ -0,0 +1,27 @@
+package com.magic.generated.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author mohuangNPC
+ * @version 1.0
+ * @date 2020/7/14 7:53
+ */
+public class MybtaisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static SqlSessionFactory init() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        if(sqlSessionFactory != null){
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        }
+        return sqlSessionFactory;
+    }
+
+}
