a3fd7c5476f1257097cb1a061d859870a11a63fc
zhaoyankai <272852115@qq.com>
2020-07-21 08:43:09 +0800

    Increase IM server

---
+++ IMServer/src/main/java/com/mohuangnpc/imserver/ImserverApplication.java
@@ -0,0 +1,13 @@
+package com.mohuangnpc.imserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImserverApplication.class, args);
+    }
+
+}
---
+++ IMServer/src/test/java/com/mohuangnpc/imserver/ImserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohuangnpc.imserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
