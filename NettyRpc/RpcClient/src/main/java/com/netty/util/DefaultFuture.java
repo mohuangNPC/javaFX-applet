48e9e435ece294aea6b86564ec2918010c3bbfcd
zhaoyankai <272852115@qq.com>
2020-08-23 08:39:20 +0800

    add netty rpc client DefaultFuture

---
+++ NettyRpc/RpcClient/src/main/java/com/netty/util/DefaultFuture.java
@@ -0,0 +1,38 @@
+package com.netty.util;
+
+import com.netty.protocol.RpcResponse;
+
+/**
+ * @author mohuangNPC
+ * @version 1.0
+ * @date 2020/7/30 13:40
+ */
+public class DefaultFuture {
+    private RpcResponse rpcResponse;
+    private volatile boolean isSucceed = false;
+    private final Object object = new Object();
+
+    public RpcResponse getRpcResponse(int timeout) {
+        synchronized (object) {
+            while (!isSucceed) {
+                try {
+                    object.wait(timeout);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return rpcResponse;
+        }
+    }
+
+    public void setResponse(RpcResponse response) {
+        if (isSucceed) {
+            return;
+        }
+        synchronized (object) {
+            this.rpcResponse = response;
+            this.isSucceed = true;
+            object.notify();
+        }
+    }
+}
