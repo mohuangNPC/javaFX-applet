41061edae5dd07b53a171898fbdd0cd86ef87a1f
zhaoyankai <272852115@qq.com>
2020-08-31 09:10:03 +0800

    add netty rpc client RpcDecoder

---
+++ NettyRpc/RpcClient/src/main/java/com/netty/util/RpcDecoder.java
@@ -0,0 +1,42 @@
+package com.netty.util;
+
+import com.netty.serializer.Serializer;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * @author mohuangNPC
+ * @version 1.0
+ * @date 2020/7/30 10:59
+ */
+public class RpcDecoder extends ByteToMessageDecoder {
+    private Class<?> clazz;
+    private Serializer serializer;
+
+    public RpcDecoder(Class<?> clazz, Serializer serializer) {
+        this.clazz = clazz;
+        this.serializer = serializer;
+    }
+
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        //因为之前编码的时候写入一个Int型，4个字节来表示长度
+        if (byteBuf.readableBytes() < 4) {
+            return;
+        }
+        //标记当前读的位置
+        byteBuf.markReaderIndex();
+        int dataLength = byteBuf.readInt();
+        if (byteBuf.readableBytes() < dataLength) {
+            byteBuf.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        //将byteBuf中的数据读入data字节数组
+        byteBuf.readBytes(data);
+        Object obj = serializer.deserialize(clazz, data);
+        list.add(obj);
+    }
+}
