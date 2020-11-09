1b09a9fc09d045c534b156009234787645ab5c4e
zhaoyankai <272852115@qq.com>
2020-11-09 13:32:15 +0800

    add package application

---
+++ BootPackage/user/src/main/java/com/game/user/Application.java
@@ -0,0 +1,16 @@
+package com.game.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mohuangNPC
+ * @version 1.0
+ * @date 2020/8/18 12:12
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
